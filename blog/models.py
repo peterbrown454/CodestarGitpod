47beda32f5d7849e1fc26ec5afda747487f0c8b5
Peter Brown <peterbrown454@gmail.com>
2024-03-03 22:54:20 +0000

    bleh

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,11 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"The title of this post is {self.title} | Written by {self.author}"
+    
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name = "comments")
@@ -21,6 +26,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
 
 
