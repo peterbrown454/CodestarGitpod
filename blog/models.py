c52d3abfb2867aa671358f897c513f80ba0c0a11
Peter Brown <peterbrown454@gmail.com>
2024-03-04 17:51:31 +0000

    hrr

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now_add=True)
+  
     class Meta:
         ordering = ["created_on"]
     def __str__(self):
@@ -26,6 +27,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ["created_on"]
 
