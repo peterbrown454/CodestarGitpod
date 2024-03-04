e848b95875c4ab678ad15963e9b6f9beb270e0a7
Peter Brown <peterbrown454@gmail.com>
2024-03-04 00:31:43 +0000

    wtf

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgres://htaphucb:YC_phU2JkNsIJnzO4SaB9pCRzR6D4xZQ@trumpet.db.elephantsql.com/htaphucb")
+os.environ.setdefault("SECRET_KEY" , "lalala")
